7b5bdbdda7cbeca3331a2daed67c2b88fa43a462
ZhangYanzhe <2712885306@qq.com>
2019-08-11 11:54:09 +0800

    重写完成

--- src/store.js
+++ src/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userIfo: {}
   },
   mutations: {
-
+    login: function (state, data) {
+      this.state.userIfo = data
+    }
   },
   actions: {
 
